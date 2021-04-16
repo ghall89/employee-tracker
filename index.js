b67e8f01263cd829aaf79e612caab80b11e55c96
Graham Hall <ghall89@me.com>
2021-04-16 19:14:39 +0000

    display table

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 // import modules
-const express = require('express');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
 //queries
-const getEmployees = 'SELECT * FROM employees;';
+const getEmployees = `SELECT * FROM employees
+											LEFT JOIN roles 
+											ON employees.role_id = roles.id
+											LEFT JOIN departments 
+											ON roles.department_id = departments.id;`;
 
 // Connect to database
 const db = mysql.createConnection(
@@ -24,16 +20,37 @@ const db = mysql.createConnection(
 		database: 'employees'
 });
 
-const sql = db.query(getEmployees, (err, employees) => {
+
+
+db.query(getEmployees, (err, employees) => {
 	if (err) {
 		console.log("There was an error");
 		return;
 	}
-	return employees;
+	
+	const table = [];
+	
+	for (let i = 0; i < employees.length; i++) {
+		
+		let managerName;
+		
+		if (employees[i].manager_id) {
+			const managerId = employees[i].manager_id;
+			managerName = employees[managerId].first_name;
+		} else {
+			managerName = 'N/A';
+		}
+		
+		row = {
+			firstName: employees[i].first_name,
+			lastName: employees[i].last_name,
+			title: employees[i].title,
+			salary: employees[i].salary,
+			manager: managerName,
+			department: employees[i].name
+		}
+		table.push(row);
+	}
+	
+	console.table(table);
 });
-
-console.log(sql);
-
-
-// start server
-// app.listen(PORT, () => console.log(`Running on port ${PORT}`))
